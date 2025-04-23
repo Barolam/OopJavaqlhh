74cce3ba949a16275ba76d27d12293332426c1f3
Barolam <lgbao6868@gmail.com>
2025-04-23 20:56:28 +0700

    QuanLyHangHoa

--- src/com/qlhh/main/AppHH.java
+++ src/com/qlhh/main/AppHH.java
@@ -1,10 +1,29 @@
 package com.qlhh.main;
 
+import java.io.PrintWriter;
+import java.util.Scanner;
+
+import com.qlhh.ui.HHMenu;
+
+
+
 public class AppHH {
 
 	public static void main(String[] args) {
-	
+		PrintWriter out;
+ 		HHMenu menu;
+ 		Scanner in;
+ 		String prompt = "->";
 
+ 		
+ 		
+ 		
+ 		out =     new PrintWriter(System.out);
+ 		in =      new Scanner(System.in);
+ 		
+ 		
+ 		
+ 		
 	}
 
 }
